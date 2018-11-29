efe1121ed2ee8513ebdd05440707852d7e087405
Maxime Turlure <maxime.turlure@grenoble-inp.org>
2018-11-29 01:05:52 +0100

    Kafka qui marche pas

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,7 +1,8 @@
+export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook ../aws_ansible/create_instances.yml
 sleep 15
 ansible-playbook -i ../aws_ansible/hosts setup.yml
 ansible-playbook -i ../aws_ansible/hosts master.yml
 ansible-playbook -i ../aws_ansible/hosts slaves.yml
-ansible-playbook -i ../aws_ansible/hosts mongodb.yml
-ansible-playbook -i ../aws_ansible/hosts kafka_ansible.yml
+#ansible-playbook -i ../aws_ansible/hosts mongo/mongodb.yml
+ansible-playbook -i ../aws_ansible/hosts kafka/kafka_ansible.yml
