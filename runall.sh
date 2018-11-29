fcc5bb4545aa14347eaa3b55816e0dd0eb10e7de
Alexandre <>
2018-11-29 15:37:31 +0100

    Improve kubernetes setup. Now possible to run the scripts multiple times

---
+++ runall.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+ansible-playbook aws_ansible/create_instances.yml
+
+ansible-playbook -i aws_ansible/hosts kubernetes/setup.yml
+ansible-playbook -i aws_ansible/hosts kubernetes/master.yml
+ansible-playbook -i aws_ansible/hosts kubernetes/slaves.yml
+
+ansible-playbook -i aws_ansible/hosts kubernetes/mongodb.yml
+
+ansible-playbook -i aws_ansible/hosts spark/setup.yml
